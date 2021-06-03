f8353fc839c1714ff1570dc3401865d11eda4d13
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-03 12:26:16 +0530

    Add files via upload

---
+++ Example 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dynamic_Example_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic d1, d2, d3;
+            d1 = 10;
+            d2 = 20;
+            d2 = "20";
+            d3 = d1 + d2;
+            Console.WriteLine(d3);
+        }
+    }
+}
