383c8dfc2d032140ce6da2633eae9a417468ef07
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-03 12:31:57 +0530

    Add files via upload

---
+++ Dynamic vs Var/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dynamic_vs_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3.14f;
+            //b = "Rohit";                                //Cannot implicitly convert type 'string' to 'float'
+            dynamic c = 10;
+            dynamic d = 3.14f;
+            d = "Rohit";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
