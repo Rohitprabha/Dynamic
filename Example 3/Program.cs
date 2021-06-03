de72b52f88b75f580528aac41eafe602483e570b
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-03 12:29:07 +0530

    Add files via upload

---
+++ Example 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dynamic_Example_3
+{
+    class Program
+    {
+        public static void Add(dynamic s1, dynamic s2)               // Method which contains dynamic parameters
+        {
+            Console.WriteLine(s1 + s2);
+        }
+        static void Main(string[] args)
+        {
+            Add("a", "b");
+            Add("Rohit ", "Dasari");
+            Add("Hello", 123);
+            Add(10, 20);
+        }
+    }
+}
