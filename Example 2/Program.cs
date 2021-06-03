569c2a50c712e18866cc4425bb5038c0fd2270cc
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-03 12:27:42 +0530

    Add files via upload

---
+++ Example 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dynamic_Example_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic stud = new Student();
+
+            stud.DisplayStudentInfo(1, "Rohit");                // run-time error, no compile-time error
+            stud.DisplayStudentInfo("1");                       // run-time error, no compile-time error
+            stud.FakeMethod();                                  // run-time error, no compile-time error
+            stud.DisplayStudentInfo(1);
+        }
+    }
+    public class Student
+    {
+        public void DisplayStudentInfo(int id)
+        {
+            Console.WriteLine("StudentInfo Method is called");
+        }
+    }
+}
